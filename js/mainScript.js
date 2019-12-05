081a95a1333bb47987c5dee5b7f04523ee783510
Jules Boris KREME <jules-boris.kreme@mowali.com>
2019-12-05 20:24:03 +0100

    build of mainScript file

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,5 +1,6 @@
-var $p = $('p');
-$p.on('click', function(event){
-    var date = new Date(event.timeStamp);
-    $p.text("you clicked on: "+ date)
-});
+$function(){
+
+    var $list, $newItemForm;
+    $list = $('ul');
+    $newItemForm = $('#newItemForm');
+}
